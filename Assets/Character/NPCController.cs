3e7de3b7e79b006d3ca09fd3b9e08b54a4197df8
AstreeSnow <145676494+AstreeSnow@users.noreply.github.com>
2024-10-22 11:47:40 +0200

    soupir

--- Assets/Character/NPCController.cs
+++ Assets/Character/NPCController.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -9,10 +11,18 @@ public class NPCController : MonoBehaviour
     [SerializeField] private GameObject target;
     
     private NavMeshAgent _agent;
+    private Animator _animator;
 
     private void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
         _agent.SetDestination(target.transform.position);
+        _animator = GetComponent<Animator>();
     }
+}
+
+private void Update ()
+{
+    _animator.SetFloat(name: "Speed",_agent.velocity.magnitude);
+
 }
